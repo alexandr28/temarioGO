a78dab3bd75ad91f5a609764370ddb78a94e3ebf
Alexandr Campos <alexandr.campos.rios@gmail.com>
2019-04-26 21:11:10 -0500

    agregando temas de condicionales y switch en GO

--- programacion/condicionales/main.go
+++ programacion/condicionales/main.go
@@ -1,5 +1,22 @@
 package main
 
+import "fmt"
+
 func main() {
+	// forma clasica de condiconal
+	if 5 > 10 {
+		fmt.Println("es verdadero")
+	}
+	fmt.Println("fin del ciclo")
+
+	// condicionales anidadas
+
+	if edad, nombre := 17, "Alexandr"; edad < 14 {
+		fmt.Println(nombre, "es un niño")
+	} else if edad < 18 {
+		fmt.Println(nombre, "es un adolecente")
+	} else {
+		fmt.Println(nombre, "es un adulto")
+	}
 
 }
--- programacion/switch/main.go
+++ programacion/switch/main.go
@@ -1,5 +1,32 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	//en Go no se usa break  y es mejor omitirlo
+	var a uint8 = 4
+	switch a {
+	case 1:
+		// fallthrough -> sigue evaluando, la siguiente expresión
+		fallthrough
+	case 2:
+		fmt.Println("ganaste un viaje")
+	case 3:
+		fallthrough
+	case 4:
+		fmt.Println("ganas una camioneta 4x4")
+	default:
+		fmt.Println("sigue intentando")
+	}
+	/*
+		Otra forma de usar Swich en GO usando condicionales en cada case
+	*/
+	switch x := 7; {
+	case x >= 0 && x <= 5:
+		fmt.Println("estas entre semana")
+	case x >= 6 && x <= 7:
+		fmt.Println("estas en fin de semana")
+	default:
+		fmt.Println("no es un dia valido")
+	}
 }
