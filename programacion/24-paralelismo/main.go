d8438e84352fe78e1e9ddf7f11ec6e89ef6125fd
Alexandr Campos <alexandr.campos.rios@gmail.com>
2019-05-08 11:28:05 -0500

    modificiaciones menores

--- programacion/24-paralelismo/main.go
+++ programacion/24-paralelismo/main.go
@@ -6,31 +6,31 @@ import (
 	"sync"
 )
 
-func main()  {
+func main() {
 	// determina el numero de procesadores que usa  gruntime
 	runtime.GOMAXPROCS(1)
 	var wg sync.WaitGroup
-	numbers:= []uint32{1424,235,1246,822,64,22,16,2,4,10}
+	numbers := []uint32{1424, 235, 1246, 822, 64, 22, 16, 2, 4, 10}
 	wg.Add(len(numbers))
 	fmt.Println("Comienza el proceso")
-	for _, v := range numbers  {
+	for _, v := range numbers {
 		go func(a uint32) {
 			defer wg.Done()
-			fmt.Println(a,EsPrimo(v))
+			fmt.Println(a, EsPrimo(a))
 		}(v)
 	}
 	wg.Wait()
 	fmt.Println("Termina el Proceso")
 }
-func EsPrimo(a uint32) bool  {
-	c:=0
+func EsPrimo(a uint32) bool {
+	c := 0
 	var i uint32
-	for i=1; i <= a; i++ {
-		if a%i ==0{
+	for i = 1; i <= a; i++ {
+		if a%i == 0 {
 			c++
 		}
 	}
-	if c==2 {
+	if c == 2 {
 		return true
 	}
 	return false
